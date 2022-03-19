70e9221b648c2feeb1ef60f1caeab6aad2d68fe1
Benno <benno_njord@hotmail.com>
2022-03-19 18:25:34 +0100

    fix entity repository configuration

--- libs/models/src/entities/user.entity.ts
+++ libs/models/src/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Property, Entity, Enum, OptionalProps } from '@mikro-orm/core';
 
+import { UserRepository } from '../repositories/user.repository';
 import { BaseEntity, BaseOptionalProps } from './base.entity';
 
 export enum UserState {
@@ -8,7 +9,7 @@ export enum UserState {
     Inactive = 'INACTIVE',
 }
 
-@Entity()
+@Entity({ customRepository: () => UserRepository })
 export class User extends BaseEntity<User> {
     [OptionalProps]: BaseOptionalProps;
 
