63c5e41e3a64b38333a1ede012bd941d55bb6cdb
Benno <benno_njord@hotmail.com>
2022-03-19 18:34:57 +0100

    prettier fix

--- apps/api/src/common/testing/create-user-session.helper.ts
+++ apps/api/src/common/testing/create-user-session.helper.ts
@@ -8,11 +8,14 @@ import { UserSession } from '../common.types';
 export function createTestUserSession(
     overrides: Partial<UserSession> = {},
 ): UserSession {
-    return mergeDeepRight({
-        userId: faker.datatype.uuid(),
-        email: faker.internet.email(),
-        state: UserState.Active,
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-    }, overrides);
+    return mergeDeepRight(
+        {
+            userId: faker.datatype.uuid(),
+            email: faker.internet.email(),
+            state: UserState.Active,
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+        },
+        overrides,
+    );
 }
--- libs/testing/src/create-user.helper.ts
+++ libs/testing/src/create-user.helper.ts
@@ -5,14 +5,20 @@ import { mergeDeepRight } from 'ramda';
 import { User, UserState } from '@libs/models';
 
 export function createTestUser(overrides: Partial<User> = {}): User {
-    return plainToInstance(User, mergeDeepRight({
-        id: overrides?.id || faker.datatype.uuid(),
-        email: overrides?.email || faker.internet.email(),
-        password: overrides?.password || faker.internet.password(),
-        state: overrides?.state || UserState.Active,
-        firstName: overrides?.firstName || faker.name.firstName(),
-        lastName: overrides?.lastName || faker.name.lastName(),
-        createdAt: overrides?.createdAt || new Date(),
-        updatedAt: overrides?.updatedAt || new Date(),
-    }, overrides));
+    return plainToInstance(
+        User,
+        mergeDeepRight(
+            {
+                id: overrides?.id || faker.datatype.uuid(),
+                email: overrides?.email || faker.internet.email(),
+                password: overrides?.password || faker.internet.password(),
+                state: overrides?.state || UserState.Active,
+                firstName: overrides?.firstName || faker.name.firstName(),
+                lastName: overrides?.lastName || faker.name.lastName(),
+                createdAt: overrides?.createdAt || new Date(),
+                updatedAt: overrides?.updatedAt || new Date(),
+            },
+            overrides,
+        ),
+    );
 }
