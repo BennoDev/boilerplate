cf4c28d7432c086f9d6788fa80fa7b0f89ec755c
Benno <benno_njord@hotmail.com>
2024-01-09 23:03:10 +0100

    Add env vars to workflow config

--- libs/logger/src/lib/logger.config.ts
+++ libs/logger/src/lib/logger.config.ts
@@ -8,13 +8,6 @@ export type LoggerConfig = {
     readonly environment: Environment;
     readonly logLevel: LogLevel;
     /**
-     * Separate LogLevel for database logs. Database debug logs take up a lot of space in the overall logs
-     * and tend to drown application logs, so this property is to be able to configure them separately.
-     *
-     * This log level can only be higher or equal to the main logLevel property, otherwise it will have no affect - as the main logLevel is always respected.
-     */
-    readonly databaseLogLevel: LogLevel;
-    /**
      * Enable middleware that will log all incoming requests and responses.
      * Also creates a request context with a trace id that will automatically be added to all logs within the same request.
      */
@@ -22,7 +15,6 @@ export type LoggerConfig = {
 };
 
 export const loggerConfig = registerAs<LoggerConfig>('logger', () => ({
-    databaseLogLevel: tryGetEnv('DATABASE_LOG_LEVEL') as LogLevel,
     enableRequestLogging: Boolean(tryGetEnv('ENABLE_REQUEST_LOGGING')),
     environment: tryGetEnv('NODE_ENV') as Environment,
     logLevel: tryGetEnv('LOG_LEVEL') as LogLevel,
