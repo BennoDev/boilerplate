051c98fc8f780241e5f4be8545913ee20da269cd
Benno Meysmans <benno.meysmans@datacamp.com>
2024-06-21 12:12:37 +0200

    Remove unnecessary param from test DB registration

--- libs/database/src/lib/database.module.ts
+++ libs/database/src/lib/database.module.ts
@@ -46,7 +46,7 @@ export class DatabaseModule {
     /**
      * Use this method to register a database module for an integration test.
      * The difference between this and `register` is that there is no
-     * custom logger implementation.
+     * custom logger implementation & we allow the global context.
      */
     static registerTest(
         entities: Array<EntityClass<AnyEntity>>,
@@ -64,7 +64,6 @@ export class DatabaseModule {
                     inject: [databaseConfig.KEY],
                     useFactory: (config: DatabaseConfig) => ({
                         ...config,
-                        entities,
                         allowGlobalContext: true,
                     }),
                 }),
