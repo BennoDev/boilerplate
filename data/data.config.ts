5da06f43afbe1e934fc5c96cf72f8a28201f1161
Benno Meysmans <benno.meysmans@datacamp.com>
2024-11-13 15:27:17 +0100

    Update migration generation code

--- data/data.config.ts
+++ data/data.config.ts
@@ -1,7 +1,7 @@
+import { readdirSync } from 'node:fs';
 import { join } from 'node:path';
-import { question } from 'readline-sync';
 
-import { Options, UnderscoreNamingStrategy } from '@mikro-orm/core';
+import { type Options, UnderscoreNamingStrategy } from '@mikro-orm/core';
 import { Migrator } from '@mikro-orm/migrations';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { SeedManager } from '@mikro-orm/seeder';
@@ -9,7 +9,6 @@ import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
 import { config } from 'dotenv-safe';
 
 import { MigrationGenerator } from './migration-generator';
-import { readdirSync } from 'node:fs';
 
 /**
  * The environments here have to be the same as mentioned in libs/common/core/src/lib/common.constants.ts.
@@ -28,13 +27,8 @@ if (!isRemoteEnvironment) {
     });
 }
 
-/**
- * Allows us to properly name our migrations rather than the default Migration<TimeStamp> MikroORM uses.
- * Courtesy of https://github.com/mikro-orm/mikro-orm/issues/914#issuecomment-815129660.
- *
- * @param path Path where the migration will be located.
- */
-const migrationFileName = (path: string): string => {
+const migrationFileName = (_timestamp: string, name?: string): string => {
+    const path = join(__dirname, 'migrations');
     const delimiter = '-';
 
     // Get the last migration
@@ -45,26 +39,6 @@ const migrationFileName = (path: string): string => {
         // Increment the counter based on the last migration
         counter = parseInt(lastFileName.split(delimiter)[0], 10) + 1;
     }
-
-    // Ask user for a short description (keep asking until you get something or they quit with ctrl+c)
-    let name = '';
-    while (!name) {
-        name = question(
-            '\nWhat is the name of the new migration (kebab-cased)\n> ',
-        )
-            // Replace any number of whitespace characters with the delimiter
-            .replace(/\s+/gi, delimiter)
-            .toLowerCase();
-    }
-
-    /**
-     * Assigning to an environment variable here so we can easily access it in other files during generation.
-     * There is no risk of conflicts as any existing variable would be overwritten here for the duration of the script.
-     */
-    process.env.MIGRATION_NAME = name;
-
-    // New line to find question in logs easier
-    console.log();
 
     return `${counter.toString().padStart(4, '0')}-${name}.migration`;
 };
@@ -92,7 +66,7 @@ const baseConfig: Options<PostgreSqlDriver> = {
         tableName: 'migrations',
         snapshot: false,
         generator: MigrationGenerator,
-        fileName: () => migrationFileName(join(__dirname, 'migrations')),
+        fileName: migrationFileName,
     },
     driver: PostgreSqlDriver,
     extensions: [Migrator, SeedManager],
@@ -115,4 +89,6 @@ const dataConfig: Options<PostgreSqlDriver> = isRemoteEnvironment
     ? { ...baseConfig, ...remoteConfig }
     : { ...baseConfig, ...localConfig };
 
+// MikroORM configuration expects a default export
+// eslint-disable-next-line import/no-default-export
 export default dataConfig;
