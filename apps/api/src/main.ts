5ea476da59bf7fe28e8c6774fbc23e2038e1633d
Benno Meysmans <benno.meysmans@datacamp.com>
2025-01-23 10:52:56 +0100

    Add comment on Prometheus OTEL vs prom investigation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,4 @@
-import './init';
+import './instrument';
 
 import {
     type INestApplication,
--- apps/worker/src/main.ts
+++ apps/worker/src/main.ts
@@ -1,4 +1,4 @@
-import './init';
+import './instrument';
 
 import { NestFactory } from '@nestjs/core';
 
