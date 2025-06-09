15d885abb72ea27d84b4a8d2f67dd974c796c1c9
Benno <benno_njord@hotmail.com>
2025-06-09 10:59:02 +0200

    Replace bcrypt with scrypt

--- apps/api/src/app/auth/services/hash.service.ts
+++ apps/api/src/app/auth/services/hash.service.ts
@@ -1,15 +1,32 @@
-import { Injectable } from '@nestjs/common';
-import { hash, compare } from 'bcrypt';
+import * as crypto from 'node:crypto';
 
-const saltRounds = 10;
+import { Injectable } from '@nestjs/common';
+
+const keyLength = 64;
 
 @Injectable()
 export class HashService {
-    hash(text: string): Promise<string> {
-        return hash(text, saltRounds);
+    async hash(text: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            const salt = crypto.randomBytes(16).toString('hex');
+
+            crypto.scrypt(text, salt, keyLength, (err, key) => {
+                if (err) reject(err);
+
+                resolve(`${salt}:${key.toString('hex')}`);
+            });
+        });
     }
 
-    compare(text: string, hashed: string): Promise<boolean> {
-        return compare(text, hashed);
+    compare(text: string, hash: string): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            const [salt, existingKey] = hash.split(':');
+
+            crypto.scrypt(text, salt, keyLength, (err, key) => {
+                if (err) reject(err);
+
+                resolve(existingKey === key.toString('hex'));
+            });
+        });
     }
 }
